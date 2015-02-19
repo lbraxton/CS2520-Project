c13373521c4e637a006a40a4f8819ad80ca0ae1e
lbraxton <lbraxton30@comcast.net>
2015-02-19 18:37:17 -0500

    FileManager class added

---
+++ SFTPProject/src/FileManager.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Lanette
+ *
+ */
+public class FileManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
